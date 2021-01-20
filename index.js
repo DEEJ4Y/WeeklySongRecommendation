d23db25623011baadc8fcd284374d791dd779a75
davidjosephind <davidjosephind@gmail.com>
2021-01-20 12:45:29 +0530

    Finalize new design

---
+++ index.js
@@ -0,0 +1,14 @@
+const songs = [
+  {
+    name: 'Cute Without the "E" (Cut from the team)',
+    band: "Taking Back Sunday",
+    album: "Tell All Your Friends",
+    appleMusic:
+      "https://music.apple.com/us/album/cute-without-the-e-cut-from-the-team-remastered/1481301025?i=1481301234",
+    spotify: "https://open.spotify.com/track/4lrEOsjmbKG6mf0buc7eny",
+    primeMusic:
+      "https://music.amazon.in/albums/B07586LYHQ?trackAsin=B075868P7W&ref=dm_sh_5f5c-bcca-dmcp-0d83-5f637&musicTerritory=IN&marketplaceId=A21TJRUUN4KGV",
+    ytMusic: "https://music.youtube.com/watch?v=l_Pj32HujgQ&feature=share",
+    ytEmbed: "https://www.youtube.com/embed/xnaFpQaGFCA",
+  },
+];
